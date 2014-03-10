662afc7ed1cd2b3c4c2c2f4b33f009ff99ed22ec
Esther j <jesther0103@gmail.com>
2014-03-10 09:19:54 -0400

    Adding files

---
+++ footer.php
@@ -0,0 +1,11 @@
+		<div id="footer">
+			&copy;<?php echo date("Y"); echo " Esther H Jung"; ?>
+		</div>
+
+	</div>
+
+	<?php wp_footer(); ?>
+	
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
+
+	<ul class="vertical_menu">
+		<li> 
+			<a href="">ABOUT ME</a>
+		</li>
+		<li>
+			<a href="">REALISM</a>
+		</li>
+		<li>
+			<a href="">DIGITAL WORKS</a>
+		</li>
+		<li>
+			<a href="">RESUME</a>
+		</li>
+		<li>
+			<a href="">CONTACT ME</a>
+		</li>
+	</ul>
+
+<div class="content_page">
+	<h1> This is a header </h1>
+	<p>Here is a content thing
+	Why isn't this showing up?</p>
+</div>
+
+<?php get_footer(); ?>
